4535e227ce12eaec9c86702d06ebc16e5005878e
RyzCode <ryzcode@gmail.com>
2014-02-03 01:01:26 +0900

    Bug fix for auto-installation when some packages are missing

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,18 +22,31 @@
 (package-initialize)
 
 ;;;----> Installation packages listed below if not yet installed
-(defvar package-list '(
-                     init-loader
-                     helm
-                     web-mode
-                   )
-  "List of packages to install at startup")
-(defun package-list-install ()
-  "Install package list \(package-list variable\) if not yet installed"
-  (dolist (p package-list)
+;;; Thanks to http://batsov.com/
+(require 'cl) ; loop requires cl to be loaded - need to review this dependency
+(defvar prelude-packages
+  '(init-loader helm web-mode gist)
+;  '(ack-and-a-half auctex clojure-mode coffee-mode deft expand-region
+;                   gist groovy-mode haml-mode haskell-mode inf-ruby
+;                   magit magithub markdown-mode paredit projectile python
+;                   sass-mode rainbow-mode scss-mode solarized-theme
+;                   volatile-highlights yaml-mode yari zenburn-theme)
+  "A list of packages to ensure are installed at launch.")
+(defun prelude-packages-installed-p ()
+  (loop for p in prelude-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+(unless (prelude-packages-installed-p)
+  ;; check for new packages (package versions)
+  (message "%s" "Emacs Prelude is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; install the missing packages
+  (dolist (p prelude-packages)
     (when (not (package-installed-p p))
       (package-install p))))
-(package-list-install)
+(provide 'prelude-packages)
+
 
 
 ;;;----> Installation of init-loader.el
