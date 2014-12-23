5c911d2e986cd5c9e647c01c2bd124bbd283cd3e
RyzCode <ryzcode@gmail.com>
2014-12-24 00:53:24 +0900

    commenting out old code

--- .emacs.d/inits/00-init.el
+++ .emacs.d/inits/00-init.el
@@ -23,6 +23,8 @@
 (setq-default tab-width 4)
 (setq default-tab-width 4)
 (setq tab-stop-list (number-sequence 4 200 4))
+;'(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60
+;                      64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))
 
 ;;;----> Disable tab
 (setq-default indent-tabs-mode nil)
