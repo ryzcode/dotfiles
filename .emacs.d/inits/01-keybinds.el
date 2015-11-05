976194417eb8678315747b27d30e15a394a9b3a8
RyzCode <ryzcode@gmail.com>
2015-11-06 00:35:51 +0900

    Reorder keybinding related setup

--- .emacs.d/inits/01-keybinds.el
+++
@@ -1,8 +0,0 @@
-(global-set-key (kbd "C-z") 'undo)
-(global-set-key (kbd "C-v") 'other-window)
-(global-set-key (kbd "M-SPC") 'other-window)       ;;Alt+Space
-(global-set-key (kbd "M-[ 1 5") 'revert-buffer)    ;;F5
-(global-set-key (kbd "M-t") 'helm-for-files)       ;;Alt+t
-(global-set-key (kbd "C-_") 'redo)                 ;;Ctrl+-
-
-
